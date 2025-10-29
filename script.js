aada78df38187c047adc2b2d2497818e6d17f872
Kacper <kacper3302009@wp.pl>
2025-10-29 10:52:59 +0100

    changing size input header elements

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navbar = document.querySelector('nav');
 const btn_location = document.querySelector('.btn-location');
 const order_list = document.querySelectorAll('.order-list');
+const order_list_container = document.querySelector('.order-list-container')
 console.log(window.scrollY);
 
 document.addEventListener('scroll', () => {
@@ -8,15 +9,19 @@ document.addEventListener('scroll', () => {
     if (scrollY > 30) {
         navbar.style.backgroundColor = 'white';
     }
-    if(scrollY >= 500){
+    else{
+      navbar.style.backgroundColor = 'transparent';
+    }
+    if(scrollY >= 700){
         btn_location.style.display = 'block';
     }
     else{
-        navbar.style.backgroundColor = 'transparent';
+        
         btn_location.style.display = 'none';
     }
 })
 order_list[0].addEventListener('click', ()=> {
     order_list[1].classList.toggle('hidden');
     order_list[2].classList.toggle('hidden');
+    
 })
