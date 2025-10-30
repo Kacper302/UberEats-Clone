5b7862ff03c3f453126c5446b57cc5810bc06400
Kacper <kacper3302009@wp.pl>
2025-10-30 10:14:30 +0100

    feature/add canceling order after click

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const order_list = document.querySelectorAll('.order-list');
 const order_list_container = document.querySelector('.order-list-container');
 
 const overlay_background = document.querySelector('.overlay');
-const overlay_cancel_btn = document.querySelector('.overlay-location-cancel');
+const overlay_cancel_location = document.querySelector('.overlay-location-cancel');
+const overlay_cancel_order = document.querySelector('.overlay-order-cancel');
+const overlay_order_now = document.querySelector('.overlay-order-now');
 
 const overlay_order = document.querySelector('.overlay-order');
 const overlay_location = document.querySelector('.overlay-location');
@@ -27,27 +29,44 @@ document.addEventListener('scroll', () => {
         btn_location.style.display = 'none';
     }
 })
+
 order_list[0].addEventListener('click', ()=> {
     order_list[1].classList.toggle('hidden');
     order_list[2].classList.toggle('hidden');
     
 })
-
+order_list[1].addEventListener('click', () => {
+    order_list[1].classList.add('hidden');
+    order_list[2].classList.add('hidden');
+})
 
 btn_location.addEventListener('click', () => {
   overlay_background.classList.add('active');
   overlay_location.style.display = 'block';
 
 })
-overlay_cancel_btn.addEventListener('click', () => {
+overlay_cancel_location.addEventListener('click', () => {
     overlay_background.classList.remove('active')
      overlay_location.style.display = 'none';
+})
+overlay_cancel_order.addEventListener('click', () => {
+    overlay_background.classList.remove('active')
+     overlay_order.style.display = 'none';
+
+     order_list[1].classList.add('hidden');
+    order_list[2].classList.add('hidden');
+})
+
+overlay_order_now.addEventListener('click', () => {
+     overlay_background.classList.remove('active')
+     overlay_order.style.display = 'none';
 })
 
 document.addEventListener('click', (e) => {
     if(e.target == overlay_background){
-    overlay_background.classList.remove('active')
+    overlay_background.classList.remove('active');
     overlay_location.style.display = 'none';
+    overlay_order.style.display = 'none';
     }
 })
 
